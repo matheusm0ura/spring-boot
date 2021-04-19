e8582560657d5c1139699310f715b37b1e4f8494
matheusm0ura <matheusdj49@live.com>
2021-04-19 02:26:46 -0300

    Product

---
+++ src/main/java/com/spring/curso/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.spring.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.curso.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
